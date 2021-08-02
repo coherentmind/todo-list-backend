1ee17b4b63d470a8b36b3c2a41b227d0eaddd886
Olof333 <fleksovich@gmail.com>
2021-08-02 16:23:37 +0300

    created endpoints for tasks

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -13,10 +13,10 @@ router.include_router(user.router, prefix="/user")
 def create_new_user(
     db: MySQLConnection = Depends(deps.get_db), *, data: schemas.UserCreate
 ) -> schemas.UserCreate:
-    data = crud.user.create(
+    res = crud.user.create(
         db, data=schemas.UserCreate(email=data.email, password=data.password)
     )
-    return data
+    return res
 
 
 @router.put("/user/")
@@ -27,3 +27,33 @@ def update_user_data(
 ) -> schemas.UserUpdate:
     crud.user.update(db, id=current_user.id, data=data)
     return schemas.Msg(msg="User was successfully updated")
+
+
+@router.post("/task/")
+def create_new_task(
+    data: schemas.TaskCreateExtended,
+    db: MySQLConnection = Depends(deps.get_db),
+    current_user: schemas.UserReturn = Depends(deps.get_current_user),
+) -> schemas.TaskCreate:
+    res = crud.task.create(db, data=schemas.TaskCreate(name=data.name))
+    return res
+
+
+@router.put("/task/")
+def update_task_data(
+    data: schemas.TaskUpdate,
+    db: MySQLConnection = Depends(deps.get_db),
+    current_user: schemas.UserReturn = Depends(deps.get_current_user),
+) -> schemas.TaskUpdate:
+    crud.task.update(db, id=current_user, data=data)
+    return schemas.Msg(msg="Task was seccessfully updated")
+
+
+@router.delete("/task/")
+def delete_task(
+    data: schemas.TaskReturn,
+    db: MySQLConnection = Depends(deps.get_db),
+    current_user: schemas.UserReturn = Depends(deps.get_current_user),
+) -> None:
+    crud.task.delete(db, id=data.id)
+    return schemas.Msg(msg="Your task was seccessfully deleted")
